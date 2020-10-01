b0b02ce1fd90e760438ae9ca1958b3c97f91c8df
MrEverything <nampp1204@gmail.com>
2020-10-01 09:37:32 +0700

    up project

---
+++ src/main/java/com/phuongnam/model/Student.java
@@ -0,0 +1,61 @@
+package com.phuongnam.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Date;
+
+@Entity
+public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String studentCode;
+    private Date dateOfBirth;
+    private int phoneNumber;
+
+    public Student() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getStudentCode() {
+        return studentCode;
+    }
+
+    public void setStudentCode(String studentCode) {
+        this.studentCode = studentCode;
+    }
+
+    public Date getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(Date dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public int getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(int phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+}
---
+++ src/main/java/com/phuongnam/service/impl/StudentServiceImpl.java
@@ -0,0 +1,7 @@
+package com.phuongnam.service.impl;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class StudentServiceImpl {
+}
